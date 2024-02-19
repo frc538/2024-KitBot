dcec3737a71fde8b3237a9e73ed6b35a264e6a55
OwenPatrik <145722866+OwenPatrik@users.noreply.github.com>
2024-02-19 15:10:41 -0600

    Add pidgeon

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,11 +4,13 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.Pigeon2;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSubsystem extends SubsystemBase {
@@ -17,12 +19,16 @@ public class DriveSubsystem extends SubsystemBase {
   private final CANSparkMax rearLeft;
   private final CANSparkMax rearRight;
   private final MecanumDrive mDrive;
+  private final Pigeon2 mGyro;
   /** Creates a new DriveSubsystem. */
   public DriveSubsystem() {
     frontLeft = new CANSparkMax(8, MotorType.kBrushless);
     frontRight = new CANSparkMax(1, MotorType.kBrushless);
     rearLeft = new CANSparkMax(5, MotorType.kBrushless);
     rearRight = new CANSparkMax(2, MotorType.kBrushless);
+    mGyro = new Pigeon2(1);
+
+    mGyro.reset();
 
     frontLeft.restoreFactoryDefaults();
     // config
@@ -51,6 +57,10 @@ public class DriveSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
+    SmartDashboard.putNumber("Yaw", mGyro.getYaw().getValueAsDouble());
+    SmartDashboard.putNumber("Pitch", mGyro.getPitch().getValueAsDouble());
+    SmartDashboard.putNumber("Roll", mGyro.getRoll().getValueAsDouble());
+
     // This method will be called once per scheduler run
   }
   public void drive(double forward, double left, double ccw) {
