1bf24410e45973e719cadbeb7a77755be81cdae3
OwenPatrik <145722866+OwenPatrik@users.noreply.github.com>
2024-02-19 14:45:30 -0600

    initial functionality

---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,59 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveSubsystem extends SubsystemBase {
+  private final CANSparkMax frontLeft;
+  private final CANSparkMax frontRight;
+  private final CANSparkMax rearLeft;
+  private final CANSparkMax rearRight;
+  private final MecanumDrive mDrive;
+  /** Creates a new DriveSubsystem. */
+  public DriveSubsystem() {
+    frontLeft = new CANSparkMax(8, MotorType.kBrushless);
+    frontRight = new CANSparkMax(1, MotorType.kBrushless);
+    rearLeft = new CANSparkMax(5, MotorType.kBrushless);
+    rearRight = new CANSparkMax(2, MotorType.kBrushless);
+
+    frontLeft.restoreFactoryDefaults();
+    // config
+    frontLeft.setInverted(false);
+    frontLeft.setIdleMode(IdleMode.kBrake);
+    frontLeft.burnFlash();
+
+    frontRight.restoreFactoryDefaults();
+    // config
+    frontRight.setInverted(true);
+    frontRight.setIdleMode(IdleMode.kBrake);
+    frontRight.burnFlash();
+
+    rearLeft.restoreFactoryDefaults();
+    rearLeft.setInverted(false);
+    rearLeft.setIdleMode(IdleMode.kBrake);
+    rearLeft.burnFlash();
+
+    rearRight.restoreFactoryDefaults();
+    rearRight.setInverted(true);
+    rearRight.setIdleMode(IdleMode.kBrake);
+    rearRight.burnFlash();
+
+    mDrive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+  public void drive(double forward, double left, double ccw) {
+  mDrive.driveCartesian(forward, left, ccw);
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSubsystem extends SubsystemBase {
+  private final CANSparkMax top;
+  private final CANSparkMax bottom;
+  /** Creates a new ShooterSubsystem. */
+  public ShooterSubsystem() {
+    top = new CANSparkMax(9, MotorType.kBrushless);
+    bottom = new CANSparkMax(4, MotorType.kBrushless);
+
+    top.restoreFactoryDefaults();
+    // config
+    top.setInverted(true);
+    top.setIdleMode(IdleMode.kBrake);
+    top.burnFlash();
+
+    bottom.restoreFactoryDefaults();
+    bottom.setInverted(true);
+    bottom.setIdleMode(IdleMode.kBrake);
+    bottom.follow(top);
+    bottom.burnFlash();
+  }
+  public void shoot() {
+    top.set(1);
+  }
+  public void intake() {
+    top.set(-0.2);
+  }
+  public void stop() {
+    top.set(0);
+  }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
